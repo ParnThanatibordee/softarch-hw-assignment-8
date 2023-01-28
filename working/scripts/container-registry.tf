190ba7df9a9153d87629bd6b5a2136a1898c7f7a
ParnThanatibordee <parn8946@gmail.com>
2023-01-28 19:32:11 +0700

    set variable

--- working/scripts/container-registry.tf
+++ working/scripts/container-registry.tf
@@ -1,9 +1,9 @@
 # Creates a container registry on Azure so that you can publish your Docker images.
 
 resource "azurerm_container_registry" "container_registry" {
-  name                = "ptFlixtube"
+  name                = var.app_name_cr
   resource_group_name = azurerm_resource_group.flixtube.name
-  location            = "southeastasia"
+  location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
 }
